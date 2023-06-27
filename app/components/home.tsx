8d92ecf69f01bb7d9e2571c485254aab3a8ba377
siwting <siwt@xjjk.com>
2023-06-27 14:57:40 +0800

    test

--- app/components/home.tsx
+++ app/components/home.tsx
@@ -18,7 +18,7 @@ import { ErrorBoundary } from "./error";
 import { getLang } from "../locales";
 
 import {
-  HashRouter as Router,
+  BrowserRouter as Router,
   Routes,
   Route,
   useLocation,
